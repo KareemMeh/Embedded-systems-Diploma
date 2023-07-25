e42cf0a70260628ffd01d2d973accca502b8bd1c
Kareem Mehaisen <87514805+KareemMeh@users.noreply.github.com>
2023-07-25 22:16:38 +0300

    Modified

--- unit-2/Assingment 1/code/ex2.c
+++ unit-2/Assingment 1/code/ex2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {   int i;
-    printf("Enteran Inteager :");
+    printf("Enter an Inteager :");
     fflush(stdin) ; fflush(stdout) ;
     scanf("%d", &i);
     printf("you enterd : %d\n", i);
